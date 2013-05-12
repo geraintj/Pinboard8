bea9c52d7194b68a3730491b18073c43d1f42a35
geraintj <geraint@geraintjones.com>
2013-05-12 09:59:59 +0100

    Initial commit

---
+++ PinboardClient/IPinboardApiAdapter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PinboardClient.Model;
+
+namespace PinboardClient
+{
+    interface IPinboardApiWrapper
+    {
+        List<Tag> GetTags();
+        List<Bookmark> GetBookmarks(string tag);
+        List<Bookmark> GetAllBookmarks();
+    }
+}
---
+++ PinboardClient/Model/Bookmark.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PinboardClient.Model
+{
+    public class Bookmark
+    {
+    }
+}
